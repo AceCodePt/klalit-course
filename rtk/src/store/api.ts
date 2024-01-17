5aa44e116e789217df0eba6a009580554e272fc7
Sagi Carmel <ace.code.pt@gmail.com>
2024-01-17 14:13:40 +0200

    chore: remove comments

--- rtk/src/store/api.ts
+++ rtk/src/store/api.ts
@@ -1,7 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
-
 // queryFn -> Return the information
 // query -> baseQuery -> Return the information
 
